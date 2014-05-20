6688c85b663044eb3e785211c660916a961a10b2
Taras101 <taras101@gmail.com>
2014-05-19 21:58:07 -0400

    working track on modified tree

--- index.js
+++ index.js
@@ -1,73 +1,66 @@
 $(document).ready(function(){
-/**
- * Created by JetBrains RubyMine.
- * User: pavanpodila
- * Date: 7/17/11
- * Time: 4:30 PM
- * To change this template use File | Settings | File Templates.
- */
 
 var treeData = {
-    name: "/",
+    name: "Root1",
     contents: [
         {
-            name: "Applications",
+            name: "",
             contents: [
-                { name: "Mail.app" },
-                { name: "iPhoto.app" },
-                { name: "Keynote.app" },
-                { name: "iTunes.app" },
-                { name: "XCode.app" },
-                { name: "Numbers.app" },
-                { name: "Pages.app" }
+                { name: "" },
+                { name: "" },
+                { name: "" },
+                { name: "" },
+                { name: "" },
+                { name: "" },
+                { name: "" }
             ]
         },
         {
-            name: "System",
+            name: "",
             contents: []
         },
         {
-            name: "Library",
+            name: "",
             contents: [
                 {
-                    name: "Application Support",
+                    name: "",
                     contents: [
-                        { name: "Adobe" },
-                        { name: "Apple" },
-                        { name: "Google" },
-                        { name: "Microsoft" }
+                        { name: "" },
+                        { name: "" },
+                        { name: "" },
+                        { name: "" }
                     ]
                 },
                 {
-                    name: "Languages",
+                    name: "",
                     contents: [
-                        { name: "Ruby" },
-                        { name: "Python" },
-                        { name: "Javascript" },
-                        { name: "C#" }
+                        { name: "" },
+                        { name: "" },
+                        { name: "" },
+                        { name: "" }
                     ]
                 },
                 {
-                    name: "Developer",
+                    name: "",
                     contents: [
-                        { name: "4.2" },
-                        { name: "4.3" },
-                        { name: "5.0" },
-                        { name: "Documentation" }
+                        { name: "" },
+                        { name: "" },
+                        { name: "" },
+                        { name: "" }
                     ]
                 }
             ]
         },
         {
-            name: "opt",
+            name: "",
             contents: []
         },
         {
-            name: "Users",
+            name: "",
             contents: [
-                { name: "pavanpodila" },
-                { name: "admin" },
-                { name: "test-user" }
+                { name: "" },
+                { name: "" },
+                { name: "" }
             ]
         }
     ]
@@ -88,11 +81,13 @@ function visit(parent, visitFn, childrenFn)
     }
 }
 
+var ui;
+
 function buildTree(containerName, customOptions)
 {
     // build the options object
     var options = $.extend({
-        nodeRadius: 5, fontSize: 12
+        nodeRadius: 6, fontSize: 26
     }, customOptions);
 
 
@@ -109,11 +104,11 @@ function buildTree(containerName, customOptions)
     });
 
     // size of the diagram
-    var size = { width:$(containerName).outerWidth(), height: totalNodes * 15};
+    var size = { width:$(containerName).outerWidth(), height: totalNodes * 20};
 
     var tree = d3.layout.tree()
         .sort(null)
-        .size([size.height, size.width - maxLabelLength*options.fontSize])
+        .size([size.height, size.width - maxLabelLength * options.fontSize])
         .children(function(d)
         {
             return (!d.contents || d.contents.length === 0) ? null : d.contents;
@@ -124,12 +119,19 @@ function buildTree(containerName, customOptions)
 
 
     /*
-        <svg>
-            <g class="container" />
-        </svg>
+     <svg>
+     <g class="container" />
+     </svg>
      */
-    var layoutRoot = d3.select(containerName)
-        .append("svg:svg").attr("width", size.width).attr("height", size.height)
+    var svgRoot = d3.select(containerName)
+        .append("svg:svg").attr("width", size.width).attr("height", size.height);
+
+    // Add the clipping path
+    svgRoot.append("svg:clipPath").attr("id", "clipper")
+        .append("svg:rect")
+        .attr('id', 'clip-rect');
+
+    var layoutRoot = svgRoot
         .append("svg:g")
         .attr("class", "container")
         .attr("transform", "translate(" + maxLabelLength + ",0)");
@@ -142,7 +144,9 @@ function buildTree(containerName, customOptions)
             return [d.y, d.x];
         });
 
-    layoutRoot.selectAll("path.link")
+    var linkGroup = layoutRoot.append("svg:g");
+
+    linkGroup.selectAll("path.link")
         .data(links)
         .enter()
         .append("svg:path")
@@ -150,12 +154,15 @@ function buildTree(containerName, customOptions)
         .attr("d", link);
 
 
+    var animGroup = layoutRoot.append("svg:g")
+        .attr("clip-path", "url(#clipper)");
+
     /*
-        Nodes as
-        <g class="node">
-            <circle class="node-dot" />
-            <text />
-        </g>
+     Nodes as
+     <g class="node">
+     <circle class="node-dot" />
+     <text />
+     </g>
      */
     var nodeGroup = layoutRoot.selectAll("g.node")
         .data(nodes)
@@ -166,6 +173,17 @@ function buildTree(containerName, customOptions)
         {
             return "translate(" + d.y + "," + d.x + ")";
         });
+
+    // Cache the UI elements
+    ui = {
+        svgRoot: svgRoot,
+        nodeGroup: nodeGroup,
+        linkGroup: linkGroup,
+        animGroup: animGroup
+    };
+
+    // Attach the hover and click handlers
+    setupMouseEvents();
 
     nodeGroup.append("svg:circle")
         .attr("class", "node-dot")
@@ -186,6 +204,79 @@ function buildTree(containerName, customOptions)
         {
             return d.name;
         });
+
+}
+
+function setupMouseEvents()
+{
+    ui.nodeGroup.on('mouseover', function(d, i)
+    {
+        d3.select(this).select("circle").classed("hover", true);
+    })
+        .on('mouseout', function(d, i)
+        {
+            d3.select(this).select("circle").classed("hover", false);
+        })
+        .on('click', function(nd, i)
+        {
+            // Walk parent chain
+            var ancestors = [];
+            var parent = nd;
+            while (!_.isUndefined(parent)) {
+                ancestors.push(parent);
+                parent = parent.parent;
+            }
+
+            // Get the matched links
+            var matchedLinks = [];
+            ui.linkGroup.selectAll('path.link')
+                .filter(function(d, i)
+                {
+                    return _.any(ancestors, function(p)
+                    {
+                        return p === d.target;
+                    });
+                })
+                .each(function(d)
+                {
+                    matchedLinks.push(d);
+                });
+
+            animateParentChain(matchedLinks);
+        });
+}
+
+function animateParentChain(links)
+{
+    var linkRenderer = d3.svg.diagonal()
+        .projection(function(d)
+        {
+            return [d.y, d.x];
+        });
+
+    // Links
+    ui.animGroup.selectAll("path.selected")
+        .data([])
+        .exit().remove();
+
+    ui.animGroup
+        .selectAll("path.selected")
+        .data(links)
+        .enter().append("svg:path")
+        .attr("class", "selected")
+        .attr("d", linkRenderer);
+
+    // Animate the clipping path
+    var overlayBox = ui.svgRoot.node().getBBox();
+
+    ui.svgRoot.select("#clip-rect")
+        .attr("x", overlayBox.x + overlayBox.width)
+        .attr("y", overlayBox.y)
+        .attr("width", 0)
+        .attr("height", overlayBox.height)
+        .transition().duration(1500)
+        .attr("x", overlayBox.x)
+        .attr("width", overlayBox.width);
 }
     $(function(){
         buildTree("#tree-container");
